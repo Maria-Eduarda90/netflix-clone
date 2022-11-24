459def0d8010dc3fb46670ce5901a186089d3314
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-23 21:04:45 -0400

    modified

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,13 @@
 import avatar from '../../img/avatar.jpg';
 import avatar2 from '../../img/avatar2.png';
 
-import { Button } from '../../components/Button';
-import { Card } from '../../components/Cards';
+import { Button } from '../../components/button';
+import { Card } from '../../components/cards';
 import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
 
 
 export function Login(){
-    
     return(
         <div className={styles.container}>
             <h1>Quem está assistindo?</h1>
