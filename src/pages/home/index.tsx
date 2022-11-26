36a022f2fe3d4a96527f48528bc5594b8b0643b4
Mary Skywalker <snowdennaruto90@gmail.com>
2022-11-25 22:29:58 -0400

    added the highlight of a netflix series

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import api from '../../api/api';
+import { FeatureMovie } from '../../components/FeaturedMovie';
 import { MovieList } from '../../components/MovieList';
 
 import styles from './styles.module.scss';
@@ -23,11 +24,19 @@ interface ListProps {
 
 export function Home(){
     const [movieList, setMovieList] = useState<ListProps[]>([]);
+    const [featuredData, setFeaturedData] = useState<any>();
 
     useEffect(() => {
         const loadAll = async () => {
             const list = await api.getHomeList();
             setMovieList(list);
+
+            const originals = list.filter(i => i.slug === 'originals');
+            const randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            const chosen = originals[0].items.results[randomChosen];
+            const chosenInfo = await api.getMovieInfo(chosen.id, 'tv');
+
+            setFeaturedData(chosenInfo);
         }
 
         loadAll();
@@ -35,7 +44,12 @@ export function Home(){
 
     return(
         <div className={styles.home}>
-            <section>
+
+            {featuredData && 
+                <FeatureMovie items={featuredData}/>
+            }
+
+            <section className={styles.lists}>
                 {movieList.map((item, key) =>{
                     return (
                         <MovieList
